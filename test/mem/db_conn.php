69d6f18985f79bba1f7766fa9846d83e18698e45
palace0261 <161999333+palace0261@users.noreply.github.com>
2025-05-08 14:25:36 +0900

    Update db_conn.php

--- test/mem/db_conn.php
+++ test/mem/db_conn.php
@@ -4,7 +4,7 @@
 $servername = "svc.sel4.cloudtype.app:32715"; #서버의 실제주소 
 $username = "root"; #mysql ID
 $password = "palace"; # 지정한  mysql password
-$dbname = "test"; #만들었던 데이터베이스 이름
+$dbname = "test"; #만들었던 데이터베이스 이름1
 // MySQL 연결
 $conn = new mysqli($servername, $username, $password, $dbname);
 // 연결 확인
