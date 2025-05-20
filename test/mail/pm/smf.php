ed3d4b3f1b3e7fef3bf2e69ff834202f849e7c52
palace0261 <161999333+palace0261@users.noreply.github.com>
2025-05-20 14:11:06 +0900

    Update smf.php

--- test/mail/pm/smf.php
+++ test/mail/pm/smf.php
@@ -8,24 +8,24 @@
 <body>
 <!-- Contact Form -->
 <div id="smf">
-    <form action="tfs.php" method="post"   enctype="multipart/form-data">
-        <input type="hidden" name="MAX_FILE_SIZE" > Send this file 2   : <input name="userfile" type="file"  accept="image/*">
+<form action="tfs.php" method="post"   enctype="multipart/form-data">
+    <input type="hidden" name="MAX_FILE_SIZE" > Send this file 2   : <input name="userfile" type="file"  accept="image/*">
 
-        
-            <div class="form-group">
-                <input type="text" name="mail" class="form-control" value="palace0261@naver.com" hidden> <!-- 받는 사람 -->
-            </div>
-            <div class="form-group">
-                <input type="text" name="subject" class="form-control" placeholder="Subject">
-            </div>
-            <div class="form-group">
-                <textarea class="form-control" name="text" rows="3" placeholder="Your Message"></textarea>
 
-            </div>
+        <div class="form-group">
+        <input type="text" name="mail" class="form-control" value="palace0261@naver.com" hidden> <!-- 받는 사람 -->
+        </div>
+        <div class="form-group">
+        <input type="text" name="subject" class="form-control" placeholder="Subject">
+        </div>
+        <div class="form-group">
+        <textarea class="form-control" name="text" rows="3" placeholder="Your Message"></textarea>
 
-                        <button class="btn btn-default" type="submit">Send Message2</button>
-                        
-        </form>
+        </div>
+
+    <button class="btn btn-default" type="submit">Send Message2</button>
+
+</form>
 </div>
 </body>
 </html>
